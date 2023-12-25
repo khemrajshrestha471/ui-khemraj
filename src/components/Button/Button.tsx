21c43f174b61724c6e5d2f669e1b179f42198044
Khemraj Shrestha <khemrajshrestha471@gmail.com>
2023-12-25 14:05:24 +0545

    Link updated

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -6,11 +6,15 @@ interface Props{
 
 const Button:FC<Props> = ({children}) => {
   return (
+    <>
 <button style={{
   padding: '5rem 10rem',
   background: 'red',
   color: 'white'
 }}>{children}</button>
+<h1>This is for testing button ui</h1>
+<a href="https://www.npmjs.com/package/ui-khemraj">Click here</a>
+</>
   )
 }
 
